aa141b5f227791b0bf6ac75a0d6fd4a546a0ca05
Bennett <bndrocks2012@gmail.com>
2020-05-26 20:44:39 -0300

    Initial

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rigidbody;
+    public KeyCode Up;
+    public KeyCode Down;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(Up))
+        {
+            
+            rigidbody.velocity = new Vector2(0, moveSpeed);
+        }
+        else if (Input.GetKey(Down))
+        {
+            
+            rigidbody.velocity = new Vector2(0, -moveSpeed);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector2(0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/bola.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bola : MonoBehaviour
+{
+    public Vector2 velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(velocidade);
+        
+    }
+    
+}
